5b492bedbde4d2f6bd02e44379d9a8ca42b2a549
Dmitry Davidyk <91990771+DmitryDavidyuk@users.noreply.github.com>
2023-07-31 00:19:49 +0300

    fix header

--- mine.js
+++ mine.js
@@ -1,7 +1,9 @@
 const menuBtn = document.querySelector(".menu__btn");
 const menuMobile = document.querySelector(".menu__list");
+const body = document.querySelector("body");
 
 menuBtn.addEventListener("click", () => {
   menuMobile.classList.toggle("menu--open");
   menuBtn.classList.toggle("open");
+  body.classList.toggle("lock");
 });
